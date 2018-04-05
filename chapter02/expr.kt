4f54d83eeab90366eddafdd371b042948cea6d40
kwon <kkdosk@naver.com>
2018-04-05 14:32:52 +0900

    expr

--- chapter02/expr.kt
+++ chapter02/expr.kt
@@ -1,3 +1,5 @@
+import java.util.TreeMap
+
 interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
@@ -21,9 +23,46 @@ fun eval_when(e: Expr) : Int =
 			throw IllegalArgumentException("Unknown expression")
 	}
 
+fun evalWithLogging(e: Expr) : Int =
+	when (e) {
+		is Num -> {
+			println("num: ${e.value}")
+			e.value
+		}
+		is Sum -> {
+			val left = evalWithLogging(e.left)
+			val right = evalWithLogging(e.right)
+			println("sum: $left + $right")
+			left + right
+		}
+		else -> throw IllegalArgumentException("Unknown expression")
+	}
+
+fun fizzBuzz(i: Int) = when {
+	i % 15 == 0 -> "FizzBuzz "
+	i % 3 == 0 -> "Fizz "
+	i % 5 == 0 -> "Buzz "
+	else -> "$i "
+}
+
+val binaryReps = TreeMap<Char, String>()
 
 fun main(args: Array<String>) {
 	println("Expr")
 	println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
-	println(eval_when(Sum(Num(1), Num(2))))
+	println(evalWithLogging(Sum(Num(1), Num(2))))
+
+	// for (i in 100 downTo 1 step 2) {
+	// 	print(fizzBuzz(i))
+	// }
+
+	for (c in 'A'..'F') {
+		val binary = Integer.toBinaryString(c.toInt())
+		binaryReps[c] = binary
+	}
+
+	for ((letter, binary) in binaryReps) {
+		println("$letter = $binary")
+	}
+
 }
